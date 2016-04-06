97bf9ef580778d50607d92948e1b18a61bb1b49f
Yarko Alexander <AAYarko.SBT@sberbank.ru>
2016-04-06 10:59:13 +0600

    Lesson 3 :: initial commit. generics have been added

---
+++ lesson3/src/main/java/ru/sberbank/sbt/traineeship/lesson3/generics/Contravariance.java
@@ -0,0 +1,36 @@
+package ru.sberbank.sbt.traineeship.lesson3.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Перенос наследования исходных типов на производные от них типы в обратном порядке
+ * @version 1.0, 05.04.2016
+ * @since 05.04.2016
+ */
+public class Contravariance {
+
+
+  List<? super Number> playerNumbers = new ArrayList<Number>() // {{ add(1);}}
+  ;
+
+
+  private Contravariance doSomeInterestingThings() {
+    // playerNumbers.add(1);
+    return this;
+  }
+
+  private void print() {
+    // final Number playerNumber = playerNumbers.get(0);
+    // System.out.println(playerNumbers.get(0));
+    // System.out.println(playerNumber);
+  }
+
+  public static void main(String[] args) {
+    new Contravariance()
+      .doSomeInterestingThings()
+      .print()
+    ;
+  }
+
+}
