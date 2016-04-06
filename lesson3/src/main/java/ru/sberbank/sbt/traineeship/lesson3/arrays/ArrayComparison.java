cf45c2b8671463ee7284a4c58d9786c76a68a3e0
Yarko Alexander <AAYarko.SBT@sberbank.ru>
2016-04-06 14:12:09 +0600

    Lesson 3 :: arrays skeleton classes have been added

---
+++ lesson3/src/main/java/ru/sberbank/sbt/traineeship/lesson3/arrays/ArrayComparison.java
@@ -0,0 +1,21 @@
+package ru.sberbank.sbt.traineeship.lesson3.arrays;
+
+/**
+ * TODO: добавить javadoc
+ *
+ * @version 1.0, 06.04.2016
+ * @since        06.04.2016
+ */
+public class ArrayComparison {
+
+
+  private void print() {
+  }
+
+  public static void main(String[] args) {
+    new ArrayComparison()
+      .print()
+    ;
+  }
+
+}
---
+++ lesson3/src/main/java/ru/sberbank/sbt/traineeship/lesson3/arrays/ArrayCopy.java
@@ -0,0 +1,21 @@
+package ru.sberbank.sbt.traineeship.lesson3.arrays;
+
+/**
+ * TODO: добавить javadoc
+ *
+ * @version 1.0, 06.04.2016
+ * @since        06.04.2016
+ */
+public class ArrayCopy {
+
+
+  private void print() {
+  }
+
+  public static void main(String[] args) {
+    new ArrayCopy()
+      .print()
+    ;
+  }
+
+}
