82c7867cc8b76cf8d156710155da02ff568048dd
Alexander Yarko <413x.413x@gmail.com>
2016-04-25 02:25:41 +0600

    Lesson 8 :: добавлены несколько примеров

--- lesson8/src/main/java/ru/sberbank/sbt/traineeship/lesson8/issues/FinalFieldIssue.java
+++ lesson8/src/main/java/ru/sberbank/sbt/traineeship/lesson8/issues/FinalFieldIssue.java
@@ -2,6 +2,7 @@ package ru.sberbank.sbt.traineeship.lesson8.issues;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.concurrent.*;
 
 /**
  * Демонстрация того, можно ли менять final поля у класса или объекта.
@@ -22,13 +23,13 @@ public class FinalFieldIssue {
       0
     );
     System.out.println(CONST_INT_VALUE);
+
     updateFinalValue(
       FinalFieldIssue.class.getDeclaredField("shouldBeConstValue"),
       0
     );
     System.out.println(shouldBeConstValue);
   }
-
 
   private static void updateFinalValue(Field field, Object newValue) throws Exception {
     field.setAccessible(true);
@@ -41,3 +42,4 @@ public class FinalFieldIssue {
   }
 
 }
+
