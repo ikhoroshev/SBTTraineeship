6121ea9836e16d4225a787860a9e20c8fa0c28b1
saalexandrov <SAAleksandrov.SBT@sberbank.ru>
2016-05-10 12:18:16 +0600

    Добавлены экраны поиска и добавления пользователей

---
+++ practise/src/main/java/ru/sberbank/services/UserService.java
@@ -0,0 +1,15 @@
+package ru.sberbank.services;
+
+import ru.sberbank.model.User;
+
+import java.util.List;
+
+/**
+ * Created by salexandrov on 09.05.2016.
+ */
+public interface UserService {
+    Iterable<User> findUsersByExample (User user);
+
+    void addUser (User user);
+    void deleteUser (Long userId);
+}
