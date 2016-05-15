da84eb8144268c1304bfaeee78838d5d8fff57ea
Raimbek Rakhimbekov <rrakhimbekov@gmail.com>
2016-05-16 02:31:37 +0800

    TestRun POJO is added

---
+++ practise/src/main/java/ru/sberbank/model/TestRun.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.sberbank.model;
+
+/**
+ *
+ * @author Raim
+ */
+class TestRun {
+  private User user;
+  private Test test;
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  public Test getTest() {
+    return test;
+  }
+
+  public void setTest(Test test) {
+    this.test = test;
+  }
+  
+}
