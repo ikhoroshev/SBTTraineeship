731d2eeac5aa1661584164db2203e47c0f56146e
Raimbek Rakhimbekov <rrakhimbekov@gmail.com>
2016-05-16 02:35:14 +0800

    TestChaper POJO is added

---
+++ practise/src/main/java/ru/sberbank/model/TestChapter.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.sberbank.model;
+
+/**
+ *
+ * @author Raim
+ */
+class TestChapter {
+  private Long id;
+  private String title;
+  private Integer position;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public Integer getPosition() {
+    return position;
+  }
+
+  public void setPosition(Integer position) {
+    this.position = position;
+  }
+  
+}
