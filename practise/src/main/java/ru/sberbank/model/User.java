16d773dab9112de835026b6dc057a9ec30603d4e
Raimbek Rakhimbekov <rrakhimbekov@gmail.com>
2016-05-16 08:12:40 +0800

    User POJO is added

--- practise/src/main/java/ru/sberbank/model/User.java
+++ practise/src/main/java/ru/sberbank/model/User.java
@@ -1,70 +1,61 @@
 package ru.sberbank.model;
 
-import javax.persistence.*;
-
 /**
  * Created by sbt-shmygin-as on 06.04.2016.
  */
-@Entity
 public class User {
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private Long id;
-    @ManyToOne(cascade=CascadeType.ALL)
-    private UserGroup group;
-    @Column(nullable = false)
-    private String firstName;
-    @Column(nullable = false)
-    private String middleName;
-    @Column(nullable = false)
-    private String lastName;
+  private Long id;
+  private String firstName;
+  private String middleName;
+  private String lastName;
+  private UserGroup userGroup;
 
-    protected User() {};
+  protected User() {};
 
-    public User(UserGroup group, String firstName, String lastName, String middleName) {
-        this.group = group;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.middleName = middleName;
-    }
+  public User(UserGroup userGroup, String firstName, String lastName, String middleName) {
+    this.userGroup = userGroup;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.middleName = middleName;
+  }
 
-    public Long getId() {
-        return id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public UserGroup getGroup() {
-        return group;
-    }
+  public UserGroup getGroup() {
+    return userGroup;
+  }
 
-    public void setGroup(UserGroup group) {
-        this.group = group;
-    }
+  public void setGroup(UserGroup userGroup) {
+    this.userGroup = userGroup;
+  }
 
-    public String getFirstName() {
-        return firstName;
-    }
+  public String getFirstName() {
+    return firstName;
+  }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
 
-    public String getMiddleName() {
-        return middleName;
-    }
+  public String getMiddleName() {
+    return middleName;
+  }
 
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
+  public void setMiddleName(String middleName) {
+    this.middleName = middleName;
+  }
 
-    public String getLastName() {
-        return lastName;
-    }
+  public String getLastName() {
+    return lastName;
+  }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 }
