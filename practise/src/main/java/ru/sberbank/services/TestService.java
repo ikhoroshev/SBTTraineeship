b68a809a43fa5e5c2d89a22076a2009ab9333f64
Raimbek Rakhimbekov <rrakhimbekov@gmail.com>
2016-05-18 09:22:51 +0800

    TestService interface is added

---
+++ practise/src/main/java/ru/sberbank/services/TestService.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.sberbank.services;
+
+import ru.sberbank.model.Test;
+
+/**
+ *
+ * @author Raim
+ */
+public interface TestService {
+  void addTest (Test test);
+}
