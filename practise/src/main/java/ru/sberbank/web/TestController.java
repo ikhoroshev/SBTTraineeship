2d592247bca6d3630ecfee2ac3c5ba85b9a51168
Veronika <veronika.golovnina@outlook.com>
2016-05-25 15:51:43 +0600

    edit TestController

--- practise/src/main/java/ru/sberbank/web/TestController.java
+++ practise/src/main/java/ru/sberbank/web/TestController.java
@@ -1,9 +1,11 @@
 package ru.sberbank.web;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import ru.sberbank.model.Test;
+import ru.sberbank.model.User;
+import ru.sberbank.model.UserGroup;
 import ru.sberbank.services.TestService;
 
 import javax.annotation.Resource;
@@ -18,12 +20,17 @@ public class TestController {
     @Resource
     private TestService testService;
 
-    @RequestMapping(value = "test/add", method = RequestMethod.GET)
-    public String initViewForm(Test test){ return "test/addTest"; }
-
-    @RequestMapping(value = "test/add", method = RequestMethod.POST)
-    public String processViewForm(Test test){
-        testService.addTest(test);
+    @RequestMapping(value = "test/find", method = RequestMethod.GET)
+    public String initViewForm(Test test) {
         return "test/addTest";
     }
+
+    @RequestMapping(value = "test/addTest", method = RequestMethod.POST)
+    public String processViewForm(Test test, Map<String, Object> model) {
+        testService.addTest(test);
+        Iterable<Test> tests = testService.getAllTest();
+        model.put("allTest", tests);
+        return "test/testsList";
+    }
+
 }
