a6d3bf3afa5362927637ada86dd5f9eae40bd339
test test <teest@test.ru>
2016-05-28 21:52:00 +0600

    add question

---
+++ practise/src/main/java/ru/sberbank/services/QuestionService.java
@@ -0,0 +1,10 @@
+package ru.sberbank.services;
+
+import ru.sberbank.model.Question;
+
+/**
+ * Created by dns on 19.05.16.
+ */
+public interface QuestionService {
+    void addQuestion (Question question);
+}
