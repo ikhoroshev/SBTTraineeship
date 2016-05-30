5144b88d94440fa3ab8f76f9145895f9c6ed6002
Raimbek Rakhimbekov <rrakhimbekov@gmail.com>
2016-05-30 13:43:13 +0800

    'PUPIL' is set as default type

--- practise/src/main/java/ru/sberbank/services/CustomAuthenticationProvider.java
+++ practise/src/main/java/ru/sberbank/services/CustomAuthenticationProvider.java
@@ -15,6 +15,7 @@ import ru.sberbank.repositories.UserRepository;
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
+import ru.sberbank.model.UserGroupType;
 
 /**
  * Created by Илья on 28.05.2016.
@@ -34,7 +35,8 @@ public class CustomAuthenticationProvider implements AuthenticationProvider  {
         final User user = userRepository.findByLoginAndPassword(login, password);
         if (user != null) {
             List<GrantedAuthority> grantedAuths = new ArrayList<>();
-            grantedAuths.add(new SimpleGrantedAuthority(user.getGroup().getUserGroupType().name()));
+            
+            grantedAuths.add(new SimpleGrantedAuthority(user.getGroup() != null ? user.getGroup().getUserGroupType().name() : UserGroupType.PUPIL.name()));
             return new UsernamePasswordAuthenticationToken(login, password, grantedAuths);
         } else {
             return null;
